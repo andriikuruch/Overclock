95c8ddbf1052efbc424c8b60823fd805bbda5423
andriikuruch <andriikuruch@gamil.com>
2021-10-20 15:58:00 +0300

    test ci

--- src/main/java/com/overclock/overclock/InitControler.java
+++ src/main/java/com/overclock/overclock/InitControler.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class C {
+public class InitControler {
 
     @GetMapping
     public String index() {
-        return "hello";
+        return "HELLO";
     }
 }
