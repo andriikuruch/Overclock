f5d67839cf20f1f3be0a2976619ce15149ec66e4
andriikuruch <andriikuruch@gamil.com>
2021-10-27 00:05:56 +0300

    tets db connection

--- src/main/java/com/overclock/overclock/InitController.java
+++ src/main/java/com/overclock/overclock/InitController.java
@@ -8,10 +8,10 @@ public class InitController {
 
     @GetMapping
     public String index() {
-        return "Hello! It's work!\n" +
-                "spring.datasource.username" + System.getProperty("spring.datasource.username")
-                + "\nserver.port=" + System.getProperty("server.port")
-                + "\nspring.datasource.username=" + System.getProperty("spring.datasource.username")
-                + "\nspring.datasource.password=" + System.getProperty("spring.datasource.password");
+        return "Hello! It's work!" +
+                "<br>spring.datasource.username=" + System.getProperty("spring.datasource.username")
+                + "<br>server.port=" + System.getProperty("server.port")
+                + "<br>spring.datasource.username=" + System.getProperty("spring.datasource.username")
+                + "<br>spring.datasource.password=" + System.getProperty("spring.datasource.password");
     }
 }
