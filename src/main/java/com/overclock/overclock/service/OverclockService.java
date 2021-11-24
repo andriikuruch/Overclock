f11f7224c8b0b615c9a36ccf11111047ac8ca3a8
JM1nd <andreykasayani@gmail.com>
2021-11-24 16:25:52 +0200

    OD21DEVWB2-45 Implemented OverclockService methods. Created OverclockServiceImplTest.

--- src/main/java/com/overclock/overclock/service/OverclockService.java
+++ src/main/java/com/overclock/overclock/service/OverclockService.java
@@ -5,9 +5,12 @@ import com.overclock.overclock.model.Overclock;
 import java.math.BigInteger;
 
 public interface OverclockService {
-    Overclock getOverclockById(BigInteger id);
-    boolean save(Overclock overclock);
+    boolean save(Overclock overclock,  BigInteger assemblyId);
     boolean update(BigInteger id, Overclock newOverclock);
-    Overclock getDefaultValues(BigInteger overclockId, BigInteger assemblyId);
+    boolean delete(BigInteger id);
+    boolean deleteByAssemblyId(BigInteger assemblyId);
+    Overclock getOverclockById(BigInteger id);
+    Overclock getDefaultOverclockValues(BigInteger assemblyId);
     boolean validate(Overclock overclock);
+
 }
--- src/test/java/com/overclock/overclock/CreateUtilities.java
+++ src/test/java/com/overclock/overclock/CreateUtilities.java
@@ -163,15 +163,28 @@ public class CreateUtilities {
                 .setGPUMemoryFrequency(BigInteger.valueOf(18000))
                 .setGPUVoltage(BigDecimal.valueOf(2.0))
                 .setRAMVoltage(BigDecimal.valueOf(2.5))
-                .setRAMTimings("5-5-5-15")
+                .setRAMTimings("15-15-15-35")
                 .setRAMFrequency(BigInteger.valueOf(3112)).build();
     }
 
     public static Overclock createBadOverclock() {
         return new Overclock.Builder()
                 .setId(BigInteger.valueOf(1))
-                .setCPUFrequency(BigDecimal.valueOf(4.0))
+                .setCPUFrequency(BigDecimal.valueOf(9.0))
                 .setGPUVoltage(BigDecimal.valueOf(2.0))
+                .setRAMFrequency(BigInteger.valueOf(3112)).build();
+    }
+
+    public static Overclock createOverclockWithWrongTimings() {
+        return new Overclock.Builder()
+                .setId(BigInteger.valueOf(1))
+                .setCPUFrequency(BigDecimal.valueOf(4.0))
+                .setCPUVoltage(BigDecimal.valueOf(1.5))
+                .setGPUCoreFrequency(BigInteger.valueOf(1500))
+                .setGPUMemoryFrequency(BigInteger.valueOf(18000))
+                .setGPUVoltage(BigDecimal.valueOf(2.0))
+                .setRAMVoltage(BigDecimal.valueOf(2.5))
+                .setRAMTimings("5-5-5-25")
                 .setRAMFrequency(BigInteger.valueOf(3112)).build();
     }
 }
