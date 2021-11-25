947ede356a870270d189966922327262676c8cd7
andriikuruch <andriikuruch@gmail.com>
2021-11-25 16:55:03 +0200

    OD21DEVWB2-47 Admin Service impl was implemented block/unblock user feature

--- src/main/java/com/overclock/overclock/service/AdminService.java
+++ src/main/java/com/overclock/overclock/service/AdminService.java
@@ -5,9 +5,6 @@ import com.overclock.overclock.model.User;
 import java.math.BigInteger;
 
 public interface AdminService {
-    User getAdminById(BigInteger id);
     boolean blockUser(BigInteger id);
     boolean unblockUser(BigInteger id);
-    User getAdminByUsername(String username);
-    boolean update(BigInteger id, User newAdmin);
 }
--- src/main/java/com/overclock/overclock/service/impl/AdminServiceImpl.java
+++ src/main/java/com/overclock/overclock/service/impl/AdminServiceImpl.java
@@ -1,9 +1,7 @@
 package com.overclock.overclock.service.impl;
 
 import com.overclock.overclock.dao.UserDAO;
-import com.overclock.overclock.model.User;
 import com.overclock.overclock.service.AdminService;
-import com.overclock.overclock.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
@@ -21,27 +19,12 @@ public class AdminServiceImpl implements AdminService {
     }
 
     @Override
-    public User getAdminById(BigInteger id) {
-        return null;
-    }
-
-    @Override
     public boolean blockUser(BigInteger id) {
-        return false;
+        return userDAO.updateUserActiveStatus(id, false);
     }
 
     @Override
     public boolean unblockUser(BigInteger id) {
-        return false;
-    }
-
-    @Override
-    public User getAdminByUsername(String username) {
-        return null;
-    }
-
-    @Override
-    public boolean update(BigInteger id, User newAdmin) {
-        return false;
+        return userDAO.updateUserActiveStatus(id, true);
     }
 }
