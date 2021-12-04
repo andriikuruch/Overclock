cf764630517bf7d30a09e510b7cfc4a1c715a912
JM1nd <andreykasayani@gmail.com>
2021-12-04 23:34:44 +0200

    OD21DEVWB2-45 Moved validation method into validation service

--- src/main/java/com/overclock/overclock/service/impl/OverclockServiceImpl.java
+++ src/main/java/com/overclock/overclock/service/impl/OverclockServiceImpl.java
@@ -5,6 +5,7 @@ import com.overclock.overclock.model.Overclock;
 import com.overclock.overclock.model.Assembly;
 import com.overclock.overclock.service.AssemblyService;
 import com.overclock.overclock.service.OverclockService;
+import com.overclock.overclock.service.ValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
@@ -16,24 +17,21 @@ import java.math.BigInteger;
 public class OverclockServiceImpl implements OverclockService {
     @Autowired
     private OverclockDAO overclockDAO;
-
     @Autowired
     private AssemblyService assemblyService;
+    @Autowired
+    private ValidationService validationService;
 
     @Override
     public boolean save(Overclock overclock,  BigInteger assemblyId) {
-        if (isValidOverclock(overclock)) {
-            return overclockDAO.save(overclock, assemblyId);
-        }
-        return false;
+        validationService.checkOverclockValidity(overclock);
+        return overclockDAO.save(overclock, assemblyId);
     }
 
     @Override
     public boolean update(BigInteger id, Overclock newOverclock) {
-        if (isValidOverclock(newOverclock)) {
-            return overclockDAO.update(id, newOverclock);
-        }
-        return false;
+        validationService.checkOverclockValidity(newOverclock);
+        return overclockDAO.update(id, newOverclock);
     }
 
     @Override
@@ -64,25 +62,5 @@ public class OverclockServiceImpl implements OverclockService {
                 .setRAMFrequency(assembly.getRam().getFrequency())
                 .setRAMTimings(assembly.getRam().getTimings())
                 .setRAMVoltage(assembly.getRam().getVoltage()).build();
-    }
-
-    @Override
-    public boolean isValidOverclock(Overclock overclock) {
-        if (overclock == null) {
-            return false;
-        }
-        if (overclock.getCPUVoltage() == null || overclock.getCPUFrequency() == null ||
-        overclock.getGPUVoltage() == null || overclock.getGPUMemoryFrequency() == null || overclock.getGPUCoreFrequency() == null ||
-        overclock.getRAMVoltage() == null || overclock.getRAMFrequency() == null || overclock.getRAMTimings() == null) {
-            return false;
-        }
-        return overclock.getCPUVoltage().doubleValue() >= 0.5 && overclock.getCPUVoltage().doubleValue() <= 3.0 &&
-                overclock.getCPUFrequency().doubleValue() >= 1.0 && overclock.getCPUFrequency().doubleValue() <= 8.0 &&
-                overclock.getGPUVoltage().doubleValue() >= 0.5 && overclock.getGPUVoltage().doubleValue() <= 3.0 &&
-                overclock.getGPUMemoryFrequency().intValue() >= 1000 && overclock.getGPUMemoryFrequency().intValue() <= 230000 &&
-                overclock.getGPUCoreFrequency().intValue() >= 1000 && overclock.getGPUCoreFrequency().intValue() <= 2000 &&
-                overclock.getRAMVoltage().doubleValue() >= 0.5 && overclock.getRAMVoltage().doubleValue() <= 3.0 &&
-                overclock.getRAMFrequency().intValue() >= 2133 && overclock.getRAMFrequency().intValue() <= 5000 &&
-                overclock.getRAMTimings().matches("([1-4]\\d-){3}[2-6]\\d");
     }
 }
