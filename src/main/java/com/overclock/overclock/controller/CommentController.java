23fe9e42e064c68b37e4f2408b2ae39da7f1b3bb
JM1nd <andreykasayani@gmail.com>
2021-12-14 16:15:11 +0200

    OD21DEVWB2-61 Implemented CommentController methods. All the methods were tested in Postman.

--- src/main/java/com/overclock/overclock/controller/CommentController.java
+++ src/main/java/com/overclock/overclock/controller/CommentController.java
@@ -1,39 +1,43 @@
 package com.overclock.overclock.controller;
 
-
 import com.overclock.overclock.model.Comment;
+import com.overclock.overclock.security.UserDetailsImpl;
 import com.overclock.overclock.service.CommentService;
-import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigInteger;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/v1/assembly/{assembly_id}/comment")
+@RequestMapping("/api/v1/assembly/{assembly_id}/comment")
 public class CommentController {
-    private CommentService commentService;
-
     @Autowired
-    public void setCommentService(CommentService commentService) {
-        this.commentService = commentService;
-    }
+    private CommentService commentService;
 
     @GetMapping
     public List<Comment> getAllComments(@PathVariable("assembly_id") BigInteger assemblyId) {
-        return null;
+        return commentService.getAllCommentsByAssemblyId(assemblyId);
     }
 
     @PostMapping
-    public String addComment(Comment comment, Authentication authentication) {
-        return null;
+    public ResponseEntity<?> addComment(@RequestBody Comment comment, @PathVariable("assembly_id") BigInteger assemblyId) {
+        UserDetailsImpl user = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        commentService.save(comment, assemblyId, user.getId());
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 
     @DeleteMapping("/{comment_id}")
-    public String deleteComment(@PathVariable("comment_id") BigInteger id) {
-        return null;
+    public boolean deleteComment(@PathVariable("comment_id") BigInteger id) {
+        return commentService.delete(id);
     }
-
-
 }
