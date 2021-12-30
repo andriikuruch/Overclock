efb9ed6ab90724ef8907e3e7b5af68b3912999d3
JM1nd <andreykasayani@gmail.com>
2021-12-30 16:46:40 +0200

    Assembly preview allowed to all users

--- src/main/java/com/overclock/overclock/security/ServerSecurityConfiguration.java
+++ src/main/java/com/overclock/overclock/security/ServerSecurityConfiguration.java
@@ -110,7 +110,8 @@ public class ServerSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/api/assembly/all").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/assembly/user_assemblies/{[\\d+]}").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/assembly/search/{[\\w+]}").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/user/{id}").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/assembly/{[\\d+]}").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/user/{[\\d+]}").permitAll()
                 .antMatchers("/api/**").authenticated();
 //                .antMatchers("/**").permitAll();
 
